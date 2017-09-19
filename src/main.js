3d30295e2a6e6ae950980cbdff9a87790244e75b
zwq <1623754384@qq.com>
2017-09-19 10:28:50 +0800

    axios拦截器

--- src/main.js
+++ src/main.js
@@ -8,14 +8,36 @@ import App from './App'
 import { ToastPlugin, LoadingPlugin, AlertPlugin } from 'vux'
 import axios from 'axios'
 import constant from './components/constant'
+import statusCode from './components/status-code'
 Vue.use(LoadingPlugin)
 Vue.use(ToastPlugin)
 Vue.use(AlertPlugin)
-Vue.use(router)
 FastClick.attach(document.body)
 axios.defaults.baseURL = constant.BASE_URL
 axios.defaults.headers.common[constant.JWT_HEADER] = localStorage.getItem(constant.JWT_HEADER)
 axios.defaults.timeout = 10000
+axios.interceptors.response.use(function (response) {
+  let result = response.data
+  if (result.code === statusCode.SUCCESS) {
+    return response
+  } else if (result.code === statusCode.LOGIN_EXPIRED) {
+    Vue.$vux.toast.text('登录超时,即将返回登录页', 'middle')
+    setTimeout((e) => {
+      router.push({
+        name: 'login'
+      })
+    }, 1900)
+  } else {
+    Vue.$vux.toast.text(result.msg, 'middle')
+    return response
+  }
+}, function (err) {
+  router.push({
+    name: 'error'
+  })
+  throw err
+  // return Promise.reject(err)
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
