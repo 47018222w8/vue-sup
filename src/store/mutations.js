227d551b1331f94bbf0984cff4502263b8a79d90
zwq <1623754384@qq.com>
2017-09-22 16:18:24 +0800

    没啥用

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,7 +1,7 @@
 const CHANGE_PART = 'CHANGE_PART'
 const SCROLL_Y = 'SCROLL_Y'
 const CHANGE_LOADING = 'CHANGE_LOADING'
-
+const CALCULATE_BRAND_HEIGHT = 'CALCULATE_BRAND_HEIGHT'
 export default {
   [CHANGE_PART](state, payload) {
     state.insInfoId = payload.insInfoId
@@ -11,5 +11,8 @@ export default {
   },
   [CHANGE_LOADING](state, payload) {
     state.isLoading = payload.isLoading
+  },
+  [CALCULATE_BRAND_HEIGHT](state, payload) {
+    state.carBrandHeights = payload.carBrandHeights
   }
 }
--- src/store/state.js
+++ src/store/state.js
@@ -6,7 +6,8 @@ const state = {
   insId: null, // 询价单id
   insInfoId: null, // 零件id
   scrollY: 0, // 滚动高度,
-  isLoading: false
+  isLoading: false, // url跳转等待图标
+  carBrandHeights: [] // 品牌高度数组
 }
 export default new Vuex.Store({
   state,
