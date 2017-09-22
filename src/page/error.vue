43628a5f4b5681db7597998dc1bd644654b24abb
zwq <1623754384@qq.com>
2017-09-22 16:14:31 +0800

    error

---
+++ src/page/error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="c-home">
+    <s-header class="c-cell" :returnName="'quoteList'" :title="'找件儿'"></s-header>
+    <p>我是错误页</p>
+    <s-footer></s-footer>
+  </div>
+</template>
+
+<script>
+import sFooter from '../components/footer'
+import sHeader from '../components/header'
+import constant from '../components/constant'
+export default {
+  components: {
+    sFooter,
+    sHeader,
+    constant
+  }
+}
+</script>
+
+<style lang="less">
+.c-home {
+  display: flex;
+  flex-direction: column;
+  .c-cell {
+    flex: 0 0 auto;
+  }
+}
+</style>
